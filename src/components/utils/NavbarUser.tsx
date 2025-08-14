71081753a2f0e9dc8c30f8a522ac64069cb69c52
athazayyan <achmadatha@mhs.usk.ac.id>
2025-08-14 11:55:46 +0700

    feat:(user)  kupva di sekitar anda

--- src/components/utils/NavbarUser.tsx
+++ src/components/utils/NavbarUser.tsx
@@ -2,6 +2,7 @@ export function NavbarUser(){
     return (
         <div className="navbar-user">
             <ul className="flex space-x-4 justify-center text-black p-4 border-2 border-black bg-white m-2 rounded-2xl">
+                <li><a href="/">Home</a></li>
                 <li><a href="/jualbeli">Jual Beli</a></li>
                 <li><a href="/informasi">Informasi</a></li>
                 <li><a href="/info-kupva">Kupva Di Sekitar Anda</a></li>
